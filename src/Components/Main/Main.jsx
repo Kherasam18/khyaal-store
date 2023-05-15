0fc219642c12f75c369cd670dd4c6fd28ad13f8b
Kherasam18 <110156213+Kherasam18@users.noreply.github.com>
2023-05-15 19:01:39 +0530

    Add files via upload

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import ProductSection from "../ProductSection/ProductSection";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <ProductSection></ProductSection>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Components/ProductSection/ProductSection.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { storeData } from "../../assets/data/dummyData";
+import ProductSectionItem from "./ProductSectionItem";
+
+const ProductSection = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-3 justify-items-center py-8 gap-4 mx-auto max-w-7xl">
+        {storeData.slice(0, 6).map((product, index) => {
+          return (
+            <div key={index}>
+              <ProductSectionItem
+                id={product.id}
+                name={product.name}
+                img={product.img}
+                text={product.text}
+                price={product.price}
+                totalPrice={product.totalPrice}
+                color={product.color}
+                size={product.size}
+              ></ProductSectionItem>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ProductSection;
